2943a8319d93b296781cc0def0111de79504e589
Patchwork <thesecretdiscord62@gmail.com>
2026-09-09 16:56:43 +0200

    Update repository owner and name in config

--- forum.config.ts
+++ forum.config.ts
@@ -25,8 +25,8 @@ export default defineForumConfig({
 
 	repo: {
 		// Omit owner/name to auto-detect when building in GitHub Actions.
-		owner: 'NotReeceHarris',
-		name: 'discussion-kit'
+		owner: 'patch-com',
+		name: 'patch-com.github.io'
 	},
 
 	// Extra header links
